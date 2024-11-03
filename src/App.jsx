82c7dff1cea009c886e21badb5219e5317b863f7
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-11-03 11:53:45 +0600

    Hero Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './sections/Header';
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Services from './sections/Services';
+import Portfolio from './sections/Portfolio';
+import Contact from './sections/Contact';
+import Footer from './sections/Footer';
+import Working from './sections/Working';
+import Testmonials from './sections/Testmonials';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Services/>
+      <Portfolio/>
+      <Working/>
+      <Testmonials/>
+      <Contact/>
+      <Footer/>
+    
     </>
   )
 }
+
+// https://www.youtube.com/watch?v=IB9jFCs8SFA
 
 export default App
---
+++ src/sections/Testmonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testmonials = () => {
+    return (
+        <div>
+            Testmonials
+        </div>
+    );
+};
+
+export default Testmonials;
